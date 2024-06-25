579872eda2b5772d30d9ac44c7c88ef482b2f277
SoftAgape, Sincerus <sincerus@hanmail.net>
2024-06-25 11:24:01 +0900

    프로그래밍언어활용 과정평가 삭제
    프로그래밍언어활용 과정평가 삭제

--- 프로그래밍언어활용_과정평가/main/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package com.softagape.demo;
-
-import java.util.List;
-
-public interface AccountRepository {
-    void loadJson(List<Account> accountList) throws Exception;
-    void saveJson(List<Account> accountList) throws Exception;
-}
