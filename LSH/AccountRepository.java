932a0d07608e046e53f232cab870d43165468b77
sunho-13 <sun001017@naver.com>
2024-06-25 14:24:46 +0900

    0

--- LSH/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package demoJson;
-
-import java.util.List;
-
-public interface AccountRepository {
-    void loadJson(List<Account> accountList) throws Exception;
-    void saveJson(List<Account> accountList) throws Exception;
-}
